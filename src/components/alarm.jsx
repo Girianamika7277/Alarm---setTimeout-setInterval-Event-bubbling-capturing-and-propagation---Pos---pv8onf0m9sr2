127361c679ea26e6722785c36132681d15eeaaa5
Anamika <116385962+Girianamika7277@users.noreply.github.com>
2023-03-01 00:06:04 +0530

    edit1

--- src/components/alarm.jsx
+++ src/components/alarm.jsx
@@ -10,16 +10,16 @@ export default function Alarm(props) {
     const [editOpen, setEditOpen] = React.useState(false)
 
     const checkAlarm = () => {
-        if(props.time == props.alarmTime){
+        if (props.time == props.alarmTime) {
             setOpen(true)
-        }    
+        }
     }
 
     const handleClose = () => {
         setOpen(false)
     }
 
-    React.useEffect(()=> {
+    React.useEffect(() => {
         checkAlarm()
     }, [])
 
@@ -31,7 +31,7 @@ export default function Alarm(props) {
         props.onSnooze()
         handleClose()
     }
-    
+
     const handleDelete = () => {
         props.onDelete()
         handleClose()
@@ -41,23 +41,23 @@ export default function Alarm(props) {
 
     return (
         <>
-            <ActiveAlarm key = {props.key} open={open} label = {props.label} alarmTime={props.alarmTime} onClose={handleClose} onSnooze={handleSnooze} onDelete={handleDelete}/>
+            <ActiveAlarm key={props.key} open={open} label={props.label} alarmTime={props.alarmTime} onClose={handleClose} onSnooze={handleSnooze} onDelete={handleDelete} />
             <Paper elevation={3} >
-            <Box display='flex' flexDirection='row' justifyContent="space-between">
-                <Typography variant="h6">{props.label}</Typography>
-                <Typography variant="h6">{props.alarmTime}</Typography>
-                <Box>
-                    <IconButton area-label="delete">
-                    <Edit id = {props.index+'-edit'} onClick={()=> setEditOpen(true)}/>
-                    <AlarmEditForm open={editOpen} alarmid= {props.index} label={props.label} alarmTime={props.alarmTime} index={props.index} onEdit={handleEdit} />
-                    </IconButton>
-                    <IconButton area-label="delete">
-                    <Delete id={props.index} onClick={props.onDelete}/>
-                    </IconButton>
+                <Box display='flex' flexDirection='row' justifyContent="space-between">
+                    <Typography variant="h6">{props.label}</Typography>
+                    <Typography variant="h6">{props.alarmTime}</Typography>
+                    <Box>
+                        <IconButton area-label="delete">
+                            <Edit id={props.index + '-edit'} onClick={() => setEditOpen(true)} />
+                            <AlarmEditForm open={editOpen} alarmid={props.index} label={props.label} alarmTime={props.alarmTime} index={props.index} onEdit={handleEdit} />
+                        </IconButton>
+                        <IconButton area-label="delete">
+                            <Delete id={props.index} onClick={props.onDelete} />
+                        </IconButton>
+                    </Box>
                 </Box>
-            </Box>
             </Paper>
-            
+
             {/* {props.time == props.alarmTime ? <ActiveAlarm open={true} label={props.label} alarmTime={props.alarmTime} /> : <ActiveAlarm open={false} label={props.label} alarmTime={props.alarmTime} />} */}
         </>
     )
