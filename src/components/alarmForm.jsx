5e49a7a63d11028df227b74c6bff464ac4594f6f
Anamika <116385962+Girianamika7277@users.noreply.github.com>
2023-03-01 00:02:57 +0530

    edit

--- src/components/alarmForm.jsx
+++ src/components/alarmForm.jsx
@@ -10,46 +10,46 @@ import DialogTitle from '@mui/material/DialogTitle';
 import Slide from '@mui/material/Slide';
 
 const Transition = React.forwardRef(function Transition(props, ref) {
-  return <Slide direction="up" ref={ref} {...props} />;
+    return <Slide direction="up" ref={ref} {...props} />;
 });
 
-export default function AlarmEditForm(props) {
-  
-    const [label, setLabel] = React.useState(props.label);
-    const [alarmTime, setAlarmTime] = React.useState(props.alarmTime);
+export default function AlarmForm(props) {
+
+    const [label, setLabel] = React.useState("");
+    const [alarmTime, setAlarmTime] = React.useState("07:30");
     return (
         <>
-        <Dialog
-        open={props.open}
-        TransitionComponent={Transition}
-        keepMounted
-        onClose={props.onClose}
-        aria-describedby="alert-dialog-slide-description"
-      >
-        <DialogTitle>Edit alarm</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-slide-description">
-          <Box display="flex" flexDirection='column'>
-          <TextField id={props.alarmid + '-label'} label="Alarm Label" variant="outlined" value={label} onChange={(event) => setLabel(event.target.value)}/>
-          <TextField
-            label="Alarm clock"
-            type="time"
-            value={alarmTime}
-            id = {props.alarmid + '-time'}
-            onChange={(event)=> setAlarmTime(event.target.value)}
-            inputProps={{
-                step: 300
-            }}
-          />
-          </Box>
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-        <Button variant="contained" id={props.alarmid + '-save'} color="primary" onClick={() => props.onEdit(label, alarmTime)}>
-            Save
-        </Button>
-        </DialogActions>
-      </Dialog>
+            <Dialog
+                open={props.open}
+                TransitionComponent={Transition}
+                keepMounted
+                onClose={props.onClose}
+                aria-describedby="alert-dialog-slide-description"
+            >
+                <DialogTitle>Add an alarm</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-slide-description">
+                        <Box display="flex" flexDirection='column'>
+                            <TextField id="label" label="Alarm Label" variant="outlined" value={label} onChange={(event) => setLabel(event.target.value)} />
+                            <TextField
+                                id="add-time"
+                                label="Alarm clock"
+                                type="time"
+                                value={alarmTime}
+                                onChange={(event) => setAlarmTime(event.target.value)}
+                                inputProps={{
+                                    step: 300
+                                }}
+                            />
+                        </Box>
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button variant="contained" id='save' color="primary" onClick={() => props.onSave(label, alarmTime)}>
+                        Save
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </>
     )
 }
